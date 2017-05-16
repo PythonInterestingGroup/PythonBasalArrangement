ec6183dd4cfebdfc514a4f6b94034240413e8388
婉卿容若 <devzheng@qq.com>
2017-05-16 14:32:06 +0800

    create Hello_5.16.py which is a basal knowleage about ‘print’

---
+++ Hello_5.16.py
@@ -0,0 +1,14 @@
+
+print("hello world")
+
+print('Are', 'you', 'kidding', 'me') # more args - split by space
+
+print('100+200=', 100+200)
+
+name = input('Please input your name: ')
+
+print('Your name is ', name)
+
+
+
+
