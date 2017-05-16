2cdda9de68d76ba31f3c5c3b662361b66acb2962
Tan <Tan@appledeMacBook-Pro-2.local>
2017-05-16 15:43:13 +0800

    常亮变量

--- Python-01-05-17/hello.py
+++ Python-01-05-17/hello.py
@@ -1,2 +1,6 @@
 #!/usr/bin/env python3
-print("hello world")
+print('''line1
+	...line2
+	...lin3''')
+# name = input ("please input your name :")
+# print("hello,",name)
---
+++ Python-01-05-17/var.py
@@ -0,0 +1,8 @@
+f = 456.789
+s1 = 'Hello, world'
+s2 = 'Hello, \'Adam\''
+s3 = r'Hello, "Bart"'
+s4 = r'''Hello,
+Lisa!'''
+s5 = r'''///n///t/////ttt///'''
+print(f,s1,s2,s3,s4,s5)
