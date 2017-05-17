897e92c425d1c75bf83592e6f89d8774a8c65f2c
Tan <Tan@appledeMacBook-Pro-2.local>
2017-05-17 09:45:59 +0800

    python list and tuple

---
+++ Python-01-05-17/list_tuple.py
@@ -0,0 +1,6 @@
+# _*_ coding:utf-8 _*_
+# list 和 tuple
+classmates = ['jx','lkt','lmd','tjk','ljh']
+print('classmates length is %d'%len(classmates))
+print('classmates first mermber is %s'%classmates[0])
+print('classmates last mermber is %s'%classmates[-1])
