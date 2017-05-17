c21fc6b2e45d35c22e695241712789ad7ab38426
JKangTan <Tan@appledeMacBook-Pro-2.local>
2017-05-17 09:56:11 +0800

    python list and tuple

--- Python-01-05-17/list_tuple.py
+++ Python-01-05-17/list_tuple.py
@@ -4,3 +4,4 @@ classmates = ['jx','lkt','lmd','tjk','ljh']
 print('classmates length is %d'%len(classmates))
 print('classmates first mermber is %s'%classmates[0])
 print('classmates last mermber is %s'%classmates[-1])
+#test 
