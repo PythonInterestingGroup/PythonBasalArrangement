0992fc5aa2a3c8f270d43058649d69a2f3af26c7
婉卿容若 <devzheng@qq.com>
2017-05-19 17:53:28 +0800

    Create LTIL_5.19.py which contains list and tuple  and condition and loop.

---
+++ LTIL_5.19.py
@@ -0,0 +1,50 @@
+# list 
+
+members = ['roni', 'xyy', 'tjk', 'xn', 'lmd', 'sx', 'dm', 'zfl', 'wjy', 'jh', 'yxy', 'lpp', 'ltt']
+print(len(menbers))
+
+menbers[0]
+menbers[-1]
+
+menbers.append('yy')
+
+print(menbers)
+
+menbers.insert(1, 'xxx')
+print(menbers)
+
+menbers.pop()
+print(menbers)
+
+menbers.pop(1)
+print(menbers)
+
+diffentTypes = [11, 'roni', True]
+
+multiList = [12, 'mars', ['boxue', '11']]
+print(multiList)
+
+# tuple
+
+personalInfo = ('roni', 'boy', 25) # 指向 can not change 
+personalInfo[0]
+t = ()
+two = (1,)
+print(two)
+
+# condition
+
+# format
+age = 20
+for age > 18:
+	print("your age is: ",age)
+elif age > 6:
+	print("辣鸡")
+else:
+	print("zz")
+
+# input return str
+
+
+# loop
+
