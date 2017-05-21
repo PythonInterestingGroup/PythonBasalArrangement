6993997a2d8bba1d51a94077abf8bb7a90eb017b
lipeipei <2577892024@qq.com>
2017-05-21 20:42:36 +0800

    学习了Number型和String型

---
+++ basic_grammar.py
@@ -0,0 +1,50 @@
+item_one = 1
+item_two = 2
+item_three = 4
+print("Hello,Python!");
+if True:
+    print("1")
+else:
+    print("2")
+total = item_one + \
+        item_two + \
+        item_three
+print(total)
+item_four = 4.5
+print(item_four)
+item_five = "name"
+print(item_five)
+days = ['Monday', 'Tuesday', 'Wednesday',
+        'Thursday', 'Friday']
+print(days)
+word = 'word'
+sentence = "这个是句子"
+paragraph = """这个一个锻炼减
+肥的护肤的后宫胡会号兑付号回复丢电话回访电话"""
+print(paragraph)
+print(sentence)
+print(word)
+# 注释
+'''
+三个单引号注释
+'''
+"""
+三个双引号注释
+"""
+# world = input("\n\nPress the enter key to exit")
+# print(world)
+# a = input("press number a ")
+# b = input("press number b")
+# c = a + b
+# print(c)
+d = e = f = 1
+print(d, e, f)
+a, b, c = 2, 'string', 4.5
+print(a, b, c)
+# del d
+# print(d)
+s = 'abcdefg'
+print(s[3:5])
+
+
+
