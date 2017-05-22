3f6e17a1c0f40c8090fd85a7404ae3e1b13fd39b
JKangTan <Tan@appledeMacBook-Pro-2.local>
2017-05-22 14:45:34 +0800

    生成器 generator 输出 杨辉三角

--- Python3-05-18/special_feature.py
+++ Python3-05-18/special_feature.py
@@ -25,6 +25,34 @@ print("123与 abc 的全排列是:",[m+n for m in "123" for n in "abc"])
 import os #导入 os 模块
 print("当前目录下文件列表:",[d for d in os.listdir('.')])
 
+print("生成器: generator \n")
+g = (x*x for x in range(10))
+for n in g:
+	print(n)
+
+def fib(max):
+	n, a, b = 0, 0, 1
+	while n < max:
+		yield b
+		a,b = b, a+b
+		n = n + 1
+	return 'done'
+
+for n in fib(6):
+	print(n)
 
 
+print("杨辉三角:")
+def triangles():
+	L = [1]
+	while  True:
+		yield L
+		L.append(0)
+		L = [L[i-1]+L[i] for i in range(len(L))]
 
+n = 0
+for t in triangles():
+	print(t)
+	n += 1
+	if n == 10:
+		break
