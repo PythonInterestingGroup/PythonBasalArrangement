1798a1f9bcecfe4903d1730f11afc24deab77077
diamonddm <1074246318@qq.com>
2017-05-23 16:00:05 +0800

    base

--- pybasedata.py
+++ pybasedata.py
@@ -219,6 +219,10 @@ print s
 s.remove(4)
 print s
 
+#两个set可以做数学意义上的交集、并集等操作
 s1 = set([1, 2, 3])
 s2 = set([2, 3, 4])
 print s1 & s2
+print s1 | s2
+
+#set和dict的唯一区别仅在于没有存储对应的value，但是，set的原理和dict一样，所以，同样不可以放入可变对象，因为无法判断两个可变对象是否相等，也就无法保证set内部“不会有重复元素”。试试把list放入set，看看是否会报错。
