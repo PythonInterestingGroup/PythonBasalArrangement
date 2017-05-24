e60e114eced210a61294ef5bfdb35b54e8d7519d
loretta <lutingtingido@163.com>
2017-05-24 16:26:27 +0800

    task03

---
+++ Occurrence.py
@@ -0,0 +1,32 @@
+﻿#-------------------------------------------------------------------------------
+# Name:        模块1
+# Purpose:
+#
+# Author:      loretta
+#
+# Created:     24/05/2017
+# Copyright:   (c) loretta 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def get_times(list1):
+    s="".join(list1)
+    d=dict()
+    for j in s:
+        if j in d.keys():
+            d[j]=d[j]+1
+        else:
+            d[j]=1
+    return d
+
+
+
+if __name__ == '__main__':
+    a=['liangjianghao is hansome','xiongyue is single dog','pangruo is gaygayde']
+    d=get_times(a)
+    for (d,x) in d.items():
+       print d+":"+str(x)
+
+
+
+
