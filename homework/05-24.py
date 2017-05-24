6ac2301fff0f0c622fb82be1819d750f317b46d2
JKangTan <Tan@appledeMacBook-Pro-2.local>
2017-05-24 11:02:20 +0800

    计算 list 中字符串 每个字符总计出现的次数

--- homework/05-24.py
+++ homework/05-24.py
@@ -1,6 +1,6 @@
 # _*_ coding:utf-8 _*_
 
-
+from functools import reduce
 a = ['liangjianghao is hansome','xiongyue is single dog','pangruo is gaygayde']
 n = 0
 count = {}
@@ -14,3 +14,19 @@ for s in a :
 			count[ch] = 1
 		
 print(count)
+
+def combine(x, y):
+	return x + y
+
+
+def counting(s):
+	count = {}
+	for ch in s:
+		if ch in count:
+			n = count[ch]
+			count[ch] = n + 1
+		else :
+			n = 0
+			count[ch] = 1
+	return count
+print(counting(str(reduce(combine, a))))
