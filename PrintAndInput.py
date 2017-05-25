9ff86bacf0ea242cf2549203be0c9d2086eec6c5
邓家祥 <techby_DJX@WangpengdeMac-mini.local>
2017-05-25 11:05:02 +0800

    print和raw_input使用

---
+++ PrintAndInput.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+print 'hello,world'
+print 'The quick brown fox','jumps over','the lazy dog'
+print 300+100
+print '300+100=',300+100
+str = raw_input(':')
+print str
+name = raw_input('please input your name:')
+print name,'welcome to china!'
+
