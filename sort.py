3b6eb5c429372b126730a7d2cd30ac628151fc8c
loretta <lutingtingido@163.com>
2017-05-26 15:35:44 +0800

    对student排序

---
+++ sort.py
@@ -0,0 +1,20 @@
+﻿#-------------------------------------------------------------------------------
+# Name:        模块1
+# Purpose:
+#
+# Author:      loretta
+#
+# Created:     26/05/2017
+# Copyright:   (c) loretta 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+from operator import itemgetter
+
+if __name__ == '__main__':
+    students =[('john','B',15),
+ ('jane','A',12),
+ ('dave','B',10),
+ ('ethan','C',20),
+ ('peter','B',20),
+ ('mike','C',16)]
+    print sorted(students, key=itemgetter(1,2))
