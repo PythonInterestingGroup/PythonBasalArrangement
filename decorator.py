344abeefc998d6d061971ee9e4e86e1992e14609
婉卿容若 <devzheng@qq.com>
2017-06-02 17:44:09 +0800

    module

--- decorator.py
+++ decorator.py
@@ -62,3 +62,12 @@ log(funcTime)()
 # 当函数的参数个数太多，需要简化时，使用functools.partial可以创建一个新的函数，这个新函数可以固定住原函数的部分参数，从而在调用时更简单
 int2 = functools.partial(int, base = 2)
 print(int2('10'))
+
+
+print("========")
+
+b = time.time()
+funcTime()
+e = time.time()
+r = e-b
+print("%s" % funcTime.__name__,"use time:%.10f" % r)
