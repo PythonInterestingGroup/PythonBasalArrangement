0121e34f44262a4f8916a81f9b149b23ebd76ead
婉卿容若 <devzheng@qq.com>
2017-06-05 15:38:36 +0800

    module 6.5

--- decorator.py
+++ decorator.py
@@ -1,11 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'time count'
+
+__author__ = 'roni'
+
+
 import functools
 import time
 
 import sys
 
+'''
 print(sys.version)
 print(sys.getdefaultencoding())
 print('我得到的'.encode('utf-8'))
+'''
 
 '''
 def log(func):
@@ -50,20 +60,23 @@ def log(func):
 		print("%s" % func.__name__,"use time:%.10f" % r)
 	return wrapper
 
-#@log
-def funcTime():
-	pass
+'''
+@log
+def funcTime(des):
+	print(des)
 
-#funcTime()
+funcTime("i zui shuai")
+'''
 
-log(funcTime)()
+#log(funcTime)()
 
-
+'''
 # 当函数的参数个数太多，需要简化时，使用functools.partial可以创建一个新的函数，这个新函数可以固定住原函数的部分参数，从而在调用时更简单
 int2 = functools.partial(int, base = 2)
 print(int2('10'))
+'''
 
-
+'''
 print("========")
 
 b = time.time()
@@ -71,3 +84,7 @@ funcTime()
 e = time.time()
 r = e-b
 print("%s" % funcTime.__name__,"use time:%.10f" % r)
+'''
+
+if __name__ == '__main__':
+	log(func)
--- module_6.2.py
+++ module_6.2.py
@@ -8,7 +8,7 @@ __author__ = "roni" # 使用__author__变量把作者写进去，这样当你公
 import sys
 
 def test():
-	args = sys.argv
+	args = sys.argv # argv变量，用list存储了命令行的所有参数,argv至少有一个元素，因为第一个参数永远是该.py文件的名称
 	if len(args) == 1:
 		print("hello world")
 	elif len(args) == 2:
@@ -19,3 +19,100 @@ def test():
 
 if __name__ == '__main__':
 	test()
+
+
+from PIL import Image
+
+im = Image.open("runloop.png")
+print(im.format, im.size, im.mode)
+im.thumbnail((200, 200))
+im.save('thumb.jpg', 'JPEG')
+
+import sys
+print(sys.path)
+sys.path.append("/modules") # 修改模块路径, 运行完失效
+print(sys.path)
+
+
+import functools
+import time
+
+def decorator_fib(func):
+	@functools.wraps(func)
+	def wrapper(*args, **kw):
+		print(func(*args, **kw))
+	return wrapper
+
+@decorator_fib
+def fibonacci(n):
+	if n < 1:
+		return
+	a,b = 1,1
+	l = []
+	for i in range(1, n+1):
+		if i == 1:
+			l = l + [a]
+		elif i == 2:
+			l = l + [b]
+		else:
+			a,b = b,a+b
+			l = l + [b]
+		i += 1
+	return l[-1]
+
+
+fibonacci(6)
+
+
+
+def fib02(n,a=1,b=1,l=[]):
+	if n == 1:
+		l = l + [a]
+		return l
+	elif n == 2:
+		l = l + [b]
+		return l
+	else:
+		a,b = b, a+b
+		l = l+[b]
+		return fib02(n-1, a,b,l)
+
+print(fib02(6)[-1])
+
+
+
+def fact(n):
+	return fib03(n, 1)
+
+def fib03(num,pro=1,a=1,b=1,l=[]):
+	if num == 1:
+		l = l+[a]
+		pro = 1
+		return l
+	elif num == 2:
+		l = l + [b]
+		pro = 1
+		return l
+	else:
+		a,b = b,a+b
+		l = l + [b]
+		pro = b
+		return fib03(num-1,pro,a,b,l)
+
+print(fact(6)[-1])
+
+
+import decorator
+
+f0 = decorator.log(fibonacci)
+f0(7)
+
+f1 = decorator.log(fib02)
+f1(7)
+
+f2 = decorator.log(fib03)
+f2(7)
+
+
+
+
