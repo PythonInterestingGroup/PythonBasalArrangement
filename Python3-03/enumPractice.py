0b94fd394156638eaff3bb5de2d9ce5edbd3d824
JKangTan <Tan@appledeMacBook-Pro-2.local>
2017-06-06 14:04:06 +0800

    更名

---
+++ Python3-03/enumPractice.py
@@ -0,0 +1,20 @@
+# _*_ coding:utf-8 _*_
+from enum import Enum,unique
+
+Month = Enum('Month', ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'))
+
+for name,member in Month.__members__.items():
+	print(name,'==>',member,',',member.value)
+
+ print(Month.Month.Jan)
+@unique
+class SEASON(Enum):
+	SPR = 0
+	SUM = 1
+	AUT = 2
+	WIN = 3
+
+season1 = SEASON.SUM
+print(season1)
+
+
---
+++ Python3-04/metaclass.py
@@ -0,0 +1,30 @@
+# _*_ coding:utf-8 _*_
+
+def fn(self, name='world'): #先定义函数
+	print("Hello, %s"%name)
+
+#使用 type()创建 Hello class
+Hello = type('Hello', (object,),dict(hello=fn))
+h = Hello()
+h.hello()
+print(type(Hello))
+print(type(h))
+
+#metaclass 是类的模,所以必须是从 type 类型派生:
+class ListMetaclass(type):
+	def __new__(cls, name, bases, attrs):
+		attrs['add'] = lambda self, value :self.append(value)
+		return type.__new__(cls, name, bases, attrs)
+
+class MyList(list, metaclass = ListMetaclass):
+	pass
+
+#加入关键字 meteclass 时,编译器在创建 MyList 时就会调用 ListMetaclass 的
+#__new__()方法来创建 __new__()方法接收到的参数一次是:
+# 1.当前准备创建的类的对象 
+# 2.类名
+# 3.类继承的父类集合
+# 4.类的方法集合
+L = MyList()
+L.add(1)
+print(L)
