0a932b77e4b8650bbc993852ecdbdca94a0af2ed
roni <1210538084@qq.com>
2017-07-10 16:06:39 +0800

    爬虫2

--- urllib01_6.21.py
+++ urllib01_6.21.py
@@ -3,12 +3,26 @@
 
 from urllib import request
 
+import chardet
+
 import sys
 
-print(sys.getdefaultencoding())
+# print(sys.getdefaultencoding())
 
 if __name__ == '__main__':
-	response = request.urlopen("https://www.baidu.com")
+	'''
+	response = request.urlopen("https://fanyi.youdao.com/")
 	html = response.read()
-	html = html.decode('utf-8')
-	print(html)
+	chardet = chardet.detect(html)
+	print(chardet)
+	'''
+	req = request.Request("http://fanyi.youdao.com/")
+	response = request.urlopen(req)
+	html = response.read() # geturl() , info() , getcode()
+	print("url:",response.geturl())
+	print("元信息:",response.info())
+	print("statusCode = ",response.getcode())
+	html = html.decode("utf-8")
+	#print(html)
+
+
